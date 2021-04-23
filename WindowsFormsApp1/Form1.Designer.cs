f129013627848931c58d24e48f6a84847f361d9b
usama <usamasohail1999@gmail.com>
2021-04-24 00:54:53 +0500

    Sql CRUD APP

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtFax = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtRegion = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtPostal = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtContactTitle = new System.Windows.Forms.TextBox();
+            this.gvCustomer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(102, 37);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(323, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(93, 220);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(332, 105);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Location = new System.Drawing.Point(102, 117);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(323, 20);
+            this.txtContactName.TabIndex = 2;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(102, 76);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(323, 20);
+            this.txtCompanyName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Customer Id";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Company Name";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Contact Name";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "City:";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(111, 376);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 10;
+            // 
+            // txtFax
+            // 
+            this.txtFax.Location = new System.Drawing.Point(313, 457);
+            this.txtFax.Name = "txtFax";
+            this.txtFax.Size = new System.Drawing.Size(100, 20);
+            this.txtFax.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(248, 457);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Fax:";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(313, 415);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 20);
+            this.txtCountry.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(38, 454);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Phone";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(111, 454);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(248, 415);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Country:";
+            // 
+            // txtRegion
+            // 
+            this.txtRegion.Location = new System.Drawing.Point(313, 376);
+            this.txtRegion.Name = "txtRegion";
+            this.txtRegion.Size = new System.Drawing.Size(100, 20);
+            this.txtRegion.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(248, 376);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Region:";
+            // 
+            // txtPostal
+            // 
+            this.txtPostal.Location = new System.Drawing.Point(111, 415);
+            this.txtPostal.Name = "txtPostal";
+            this.txtPostal.Size = new System.Drawing.Size(100, 20);
+            this.txtPostal.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(38, 422);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Postal Code:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Green;
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdd.Location = new System.Drawing.Point(40, 509);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(79, 44);
+            this.btnAdd.TabIndex = 21;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Violet;
+            this.btnCancel.Location = new System.Drawing.Point(376, 509);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(79, 44);
+            this.btnCancel.TabIndex = 22;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnUpdate.Location = new System.Drawing.Point(271, 509);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(79, 44);
+            this.btnUpdate.TabIndex = 23;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(152, 509);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(79, 44);
+            this.btnDelete.TabIndex = 24;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(29, 158);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 13);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Contact Title";
+            // 
+            // txtContactTitle
+            // 
+            this.txtContactTitle.Location = new System.Drawing.Point(102, 155);
+            this.txtContactTitle.Name = "txtContactTitle";
+            this.txtContactTitle.Size = new System.Drawing.Size(323, 20);
+            this.txtContactTitle.TabIndex = 26;
+            // 
+            // gvCustomer
+            // 
+            this.gvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvCustomer.Location = new System.Drawing.Point(443, 40);
+            this.gvCustomer.Name = "gvCustomer";
+            this.gvCustomer.Size = new System.Drawing.Size(316, 458);
+            this.gvCustomer.TabIndex = 25;
+            this.gvCustomer.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GvCustomer_CellClick);
+            this.gvCustomer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 565);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtContactTitle);
+            this.Controls.Add(this.gvCustomer);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPostal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtRegion);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtFax);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtFax;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtRegion;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtPostal;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtContactTitle;
+        private System.Windows.Forms.DataGridView gvCustomer;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source=MDT-0136;Initial Catalog=db1;Integrated Security=True ");
+        string Customer_Id = String.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String InsertQuery = "insert into [dbo].[Customer] (CustomerId,Company_Name, Contact_Name, Contact_Title, Addres, City , Postal_code,Phone, Religion,Country, Fax) Values ('"+txtId.Text+"' ,'" + txtCompanyName.Text + "' ,'" + txtContactName.Text + "','" + txtContactTitle.Text + "','" + txtAddress.Text + "','" + txtCity.Text + "','" + txtPostal.Text + "','" + txtPhone.Text + "' ,'" + txtRegion.Text + "','" + txtCountry.Text + "','" + txtFax.Text + "')";
+            SqlCommand command = new SqlCommand(InsertQuery, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            BindGrid();
+        }
+        public void LoadData()
+        {
+            gvCustomer.Refresh();
+            gvCustomer.ClearSelection();
+        }
+        public void BindGrid()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select * from Customer", connection);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            gvCustomer.DataSource = ds.Tables[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindGrid();
+
+        }
+
+
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            String InsertQuery = "Delete from Customer where CustomerId =" + Customer_Id;
+            SqlCommand command = new SqlCommand(InsertQuery, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            BindGrid();
+        }
+        
+        private void GvCustomer_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            string Company_Name = gvCustomer.Rows[e.RowIndex].Cells[1].Value.ToString();
+            string Contact_Name = gvCustomer.Rows[e.RowIndex].Cells[2].Value.ToString();
+            string Contact_Title = gvCustomer.Rows[e.RowIndex].Cells[3].Value.ToString();
+            string Address = gvCustomer.Rows[e.RowIndex].Cells[4].Value.ToString();
+            string City = gvCustomer.Rows[e.RowIndex].Cells[5].Value.ToString();
+            string Postal_Code = gvCustomer.Rows[e.RowIndex].Cells[6].Value.ToString();
+            string Phone = gvCustomer.Rows[e.RowIndex].Cells[7].Value.ToString();
+            string Region = gvCustomer.Rows[e.RowIndex].Cells[8].Value.ToString();
+            string Country = gvCustomer.Rows[e.RowIndex].Cells[9].Value.ToString();
+            string Fax = gvCustomer.Rows[e.RowIndex].Cells[10].Value.ToString();
+
+            Customer_Id = gvCustomer.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+            txtCompanyName.Text = Company_Name;
+            txtContactName.Text = Contact_Name;
+            txtContactTitle.Text = Contact_Title;
+            txtAddress.Text = Address;
+            txtCity.Text = City;
+            txtPostal.Text = Postal_Code;
+            txtPhone.Text = Phone;
+            txtRegion.Text = Region;
+            txtCountry.Text = Country;
+            txtFax.Text = Fax;
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            String DeleteQuery = "Update Customer set Company_Name = '" + txtCompanyName.Text + "' ,Contact_Name = '" + txtContactName.Text + "',Contact_Title = '" + txtContactTitle.Text + "',Addres = '" + txtAddress.Text + "', City ='" + txtCity.Text + "',Postal_code = '" + txtPostal.Text + "',Phone ='" + txtPhone.Text + "' ,Religion = '" + txtRegion.Text + "',Country = '" + txtCountry.Text + "', Fax ='" + txtFax.Text + "' where CustomerID = " + Customer_Id;
+            SqlCommand command = new SqlCommand(DeleteQuery, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            BindGrid();
+        }
+    }
+}
